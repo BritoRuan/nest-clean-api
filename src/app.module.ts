639192b331c0cc054d5e2d13aefc2501ee017363
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-11-11 21:51:00 -0300

    chore: added account controllers, questions controllers and import env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
+import { CreateAccountController } from './controllers/accountControllers/create-account.controller'
+import { AuthenticateController } from './controllers/authenticateControllers/authenticate.controller'
+import { CreateQuestionController } from './controllers/questionControllers/create-question.controller'
+import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { envSchema } from './env'
+import { FetchRecentQuestionsController } from './controllers/questionControllers/fetch-recent-questions.controller'
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+    FetchRecentQuestionsController,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
