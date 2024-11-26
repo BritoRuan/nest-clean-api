4df3f88d887c5e0ec65ab34717a97b58e3efcdb5
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-11-26 14:37:31 -0300

    feat: added new controllers and modules

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,17 +1,26 @@
+import { Module } from '@nestjs/common'
+import { AuthenticateStudentUseCase } from '@/domain/forum/application/useCases/student/authenticateStudentUseCase/authenticateStudentUseCase'
 import { DatabaseModule } from '../database/database.module'
 import { CreateAccountController } from './controllers/accountControllers/create-account.controller'
 import { AuthenticateController } from './controllers/authenticateControllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/questionControllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/questionControllers/fetch-recent-questions.controller'
-import { Module } from '@nestjs/common'
+import { RegisterStudentUseCase } from '@/domain/forum/application/useCases/student/registerStudentUseCase/registerStudentUseCase'
+import { CryptographyModule } from '../cryptography/cryptography.module'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
+  ],
+  providers: [
+    CreateAccountController,
+    FetchRecentQuestionsController,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase,
   ],
 })
 export class HttpModule {}
