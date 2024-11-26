33e96d1fabc6523cf2cadb73eb24bc7b5a948b39
Ruan Brito <ruan.desenvolvedor@gmail.com>
2024-11-26 18:09:14 -0300

    chore: updated config app module and main file

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -2,7 +2,8 @@ import { AuthModule } from './auth/auth.module'
 import { HttpModule } from './http/http.module'
 import { ConfigModule } from '@nestjs/config'
 import { Module } from '@nestjs/common'
-import { envSchema } from './env'
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { envSchema } from './env'
     }),
     AuthModule,
     HttpModule,
+    EnvModule,
   ],
 })
 export class AppModule {}
